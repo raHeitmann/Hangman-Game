ecaa063151ce95bc639adff41b7feb13c3af0a16
Ryan Arnett <ryanglennarnett@gmail.com>
2017-04-22 14:03:23 -0500

    learnin

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,55 +23,42 @@ var movies = [
 ] ;
 
 
-//this will begin the game upon the pressing of space bar
-
-
-        	
 
  //generates a random y value to determine the move
 
 
         var y = Math.floor(Math.random() * 5);
 
-//loops until correct equals amount of letters in movie
+//loops until correct equals amount of letters in movie or misses = 5
 
     	for (var i = 0  ; misses < 5 && totalCorrect < movies[y].length ; i ++)
     	{
 
- //resets the correct check at 0 for the next input
+ 
     		
 
     		 alert("I'm thinking of a movie! guess what it is!");
     	
-
-
     		 alert(movies[y]);
 
-
-
+     //here is our function that will check if the letter you typed is within the movie name
 
     		function check (x)
     		{
     			correct = 0;
 
-
-
     			for (var j = 0 ; j < movies[y].length; j ++)
     			{
-    				//if input matches any letter in movies[y], correct will be greater than 0
+    			
+    			//if input matches any letter in movies[y], correct will be greater than 0
 
 	    			if (movies[y][j] === x)
 	    				{
 	    					correct ++ ;
 	    					console.log(movies[y][j]);
 	    					totalCorrect ++ ;
-
-
-	    
-
 	    				}
-
-	    		}
+				}
 	    			
 		//if correct is greater than 0 (at least one letter hit), will return as succes
 		 	if (correct > 0 )
