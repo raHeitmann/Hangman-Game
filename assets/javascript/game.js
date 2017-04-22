305743c5f4b10088a14e9d6d268b8467991f7a7c
Ryan Arnett <ryanglennarnett@gmail.com>
2017-04-22 12:02:42 -0500

    its actually reading my input omg

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,26 +37,61 @@ alert("hopefully we get this far");
  //generates a random y value to determine the move
 
 
-        var y = Math.floor(Math.random() * 5);
+        var y = Math.floor(Math.random() * 5)
 
 //loops until correct equals amount of letters in movie
 
-    	for (var i = 0 ; correct < movies[y].length || misses === 5 ; i ++){
+    	for (var i = 0  ; correct < movies[y].length || misses === 5 ; i ++)
+    	{
 
+    		correct = 0;
 
     		 alert("I'm thinking of a movie! guess what it is!");
     	
 
+
+    		 alert(movies[y]);
+
+
+
+
     		function check (x)
     		{
-    			if movies[y][i] == prompt();
-    				{
-    					correct = correct + 1 ;
-    				}
+    			correct = 0;
+
+    			for (var j = 0 ; j < movies[y].length ; j ++)
+    			{
+    				//if input matches any letter in movies[y], correct will be greater than 0
+
+	    			if (movies[y][j] == x)
+	    				{
+	    					correct = correct + 1 ;
+	    				}
+
+	    			//if correct is greater than 0 (at least one letter hit), will return as succe
+	    		}
+	    			 //resets the correct check at 0 for the next input
+
+		 	if (correct > 0 )
+			{
+				alert("you got that letter");
+			}
+
+			//otherwise, adds one to misses, will end when misses = 5
+
+		else
+			{
+				misses = misses + 1;
+			}
+	    			
 
     		}
 
-    }
-}
+    		check(prompt());
 
-}
+    		
+
+    }
+
+
+
