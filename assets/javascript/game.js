b43805b7d63deb50b8fbc5056d3f79ff844a8a2e
Ryan Arnett <ryanglennarnett@gmail.com>
2017-04-24 20:32:03 -0500

    little yb little

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,11 +10,7 @@ var totalCorrect = 0;
 
 
 
-
-
-
 //an array within an array, that will hold our movie names
-
 
 var movies = [
 	["p","u","l","p","f","i","c","t","i","o","n"],
@@ -27,76 +23,75 @@ var movies = [
 ] ;
 
 
+var y = Math.floor(Math.random() * 5);
 
- //RUNS GAME ,generates a random y value to determine the move
-
-function runGame() {
-
-        var y = Math.floor(Math.random() * 5);
-
-        console.log("I've chosen a movie! " + movies[y]);
 
 //loops until correct equals amount of letters in movie or misses = 5
 
-    		 console.log("I'm thinking of a movie! guess what it is!");
-    	
-    		 console.log(movies[y]);
 
      //here is our function that will check if the letter you typed is within the movie name
 
-    		alert(misses);
-    		alert(totalCorrect);
-    		
-    	
-    		return y;
-   		 };
+   
 
 
-    if (totalCorrect===movies[y].length) {
+    		if (totalCorrect===movies[y].length) {
 
-    	alert("you win!");
-    }
+		    	alert("you win!");
+		    }
 
-    else {
+		    else if (misses===5)
+		    {
 
-	    alert("you lose!"); 
-	}
+			    alert("you lose!"); 
+			}
 
 
-					function check (x)
-					    		{
+   	
 
-					    			var guess = document.getElementsByClass("guessLetter");
+ 
 
-					    			correct = 0;
+ console.log(movies[y]);
 
-					    			for (var j = 0 ; j < movies[y].length; j ++)
-					    			{
-					    			
-					    			//if input matches any letter in movies[y], correct will be greater than 0
+		function check (x)
+		    		{
 
-						    			if (movies[y][j] === x)
-						    				{
-						    					correct ++ ;
-						    					console.log(movies[y][j]);
-						    					totalCorrect ++ ;
-						    				}
-									}
-						    			
-							//if correct is greater than 0 (at least one letter hit), will return as succes
-							 	if (correct > 0 )
-								{
-									alert("you got that letter");
-								}
+		    			var length = movies[y].length;
 
-								//otherwise, adds one to misses, will end when misses = 5
+		    			correct = 0;
 
-							else
-								{
-									misses = misses + 1;
-								}
-						    			
+		    			for (var j = 0 ; j < length; j ++)
+		    			{
 
-}
+		    			
+		    			//if input matches any letter in movies[y], correct will be greater than 0
+
+			    			if (movies[y][j] === x)
+			    				{
+			    					correct ++ ;
+			    					alert(movies[y][j]);
+			    					totalCorrect ++ ;
+			    				}
+						}
+			    			
+				//if correct is greater than 0 (at least one letter hit), will return as succes
+				 	if (correct > 0 )
+					{
+						alert("you got that letter");
+					}
+
+					//otherwise, adds one to misses, will end when misses = 5
+
+				else
+					{
+						misses = misses + 1;
+						alert("you missed one!")
+					}
+			    			
+
+
+			    				alert(misses);
+								alert(totalCorrect);
+
+					};
 
 
