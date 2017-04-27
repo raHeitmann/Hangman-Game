bd26a939090faf9ed08dfb02a3103be4f7d850ea
Ryan Arnett <ryanglennarnett@gmail.com>
2017-04-27 15:40:12 -0500

    LOL

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,22 +31,83 @@ var movies = [
 function begin() {
 
 alert("we have begun");
-alert(movies[y]);
+
 
 number = Math.floor(Math.random() * 5);
 y = number;
 
+totalCorrect = 0;
+
+console.log(movies[y]);
+
+misses = 0;
+
+
+
+
+		var input = document.getElementById('guessLetter');
+		  input.addEventListener('onkeyup',function(){
+
+		input = input.value;
+
+		console.log(input);
+
+		check(input);
+
+				function check (x)
+				    		{
+				    		
+			    			correct = 0;
+
+			    			var length = movies[y].length;
+
+			    			console.log(x + "this should be the letter you inputted");
+
+				    			for (var j = 0 ; j < length; j ++)
+				    			{
+				    			//if input matches any letter in movies[y], correct will be greater than 0
+
+				    				console.log("we are in the checking loop");
+
+
+					    			if (toString(movies[y][j]) === x)
+					    				{
+					    					correct ++ ;
+					    					
+					    					totalCorrect ++ ;
+					    				}
+								}
+					    			
+						//if correct is greater than 0 (at least one letter hit), will return as succes
+						 	if (correct > 0 )
+							{
+								alert("you got that letter");
+							}
+
+							//otherwise, adds one to misses, will end when misses = 5
+
+						else
+							{
+								misses = misses + 1;
+								alert("you missed one!")
+							}
+					    			
+
+
+					    				alert(misses);
+										alert(totalCorrect);
+
+							};
+						});
+					
 
 
 
 
 
-				for (i=0; misses < 5 && totalCorrect < movies[y].length; i++) {
 
-				alert("we are in the loop");
-				check("guessLetter");
 
-				}
+						
 
     		if (totalCorrect===movies[y].length) {
 
@@ -73,59 +134,5 @@ y = number;
      //here is our function that will check if the letter you typed is within the movie name
 
    
-
-
-
-
-		function check (x)
-		    		{
-		    			correct = 0;
-
-		    			var x = document.getElementById("guessLetter");
-		    			x = toString(x);
-		    			
-
-		    			
-		    			var length = movies[y].length;
-
-		    			alert(x);
-
-		    			
-
-		    			for (var j = 0 ; j < length; j ++)
-		    			{
-		    			//if input matches any letter in movies[y], correct will be greater than 0
-
-		    				alert("we are in the checking loop");
-
-
-			    			if (toString(movies[y][j]) === x)
-			    				{
-			    					correct ++ ;
-			    					
-			    					totalCorrect ++ ;
-			    				}
-						}
-			    			
-				//if correct is greater than 0 (at least one letter hit), will return as succes
-				 	if (correct > 0 )
-					{
-						alert("you got that letter");
-					}
-
-					//otherwise, adds one to misses, will end when misses = 5
-
-				else
-					{
-						misses = misses + 1;
-						alert("you missed one!")
-					}
-			    			
-
-
-			    				alert(misses);
-								alert(totalCorrect);
-
-					};
 
 
